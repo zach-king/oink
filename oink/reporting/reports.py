77acc2613a4ff66850122fbe1ee8a76fc460ee1c
Zachary King <zking@firstorion.com>
2017-06-10 22:20:30 -0500

    text report implemented

--- oink/reporting/reports.py
+++ oink/reporting/reports.py
@@ -6,7 +6,8 @@ Contains handlers for reporting commands.
 Reports curated budgeting information to file.
 '''
 
-from .. import accounts, budget, transactions, db
+from .. import accounts, budget, transactions, db, colorize
+from . import _txt
 
 import os
 
@@ -19,7 +20,7 @@ VALID_FORMATS = ('txt', 'csv', 'pdf', 'html')
 def report(acct, path, fmt='txt'):
     '''Root handler for the report command'''
     if not accounts.exists(acct):
-        print('Account `{}` does not exist.'.format(acct))
+        print(colorize.color_error('[error]') + ' Account `{}` does not exist.'.format(acct))
         return
 
     # Check if file exists already
@@ -30,12 +31,12 @@ def report(acct, path, fmt='txt'):
     dirpath, filepath = os.path.split(path)
     if dirpath == '': # File in same directory
         if os.path.isfile(filepath):
-            certain = input('The file `{}` already exists.'.format(filepath) + \
-                ' Are you sure you want to overwrite it? (y/n): ').lower()
-            if not certain:
-                print('Report generation cancelled')
+            overwrite = input(colorize.color_warning('File already exists. Overwrite it? (Y/N): ')).lower()
+            if overwrite in ('n', 'no', '0'):
+                print(colorize.color_info('Report generation cancelled'))
                 return
         else:
+            # File doesn't exist so create it
             with open(path, 'w'):
                 pass
 
@@ -44,10 +45,9 @@ def report(acct, path, fmt='txt'):
             with open(path, 'w'):
                 pass
         else:
-            certain = input('The file `{}` already exists.'.format(path) + \
-                ' Are you sure you want to overwrite it? (y/n): ').lower()
-            if not certain:
-                print('Report generation canceleld')
+            overwrite = input(colorize.color_warning('File already exists. Overwrite it? (Y/N): ')).lower()
+            if overwrite in ('n', 'no', '0'):
+                print(colorize.color_info('Report generation cancelled'))
                 return
 
     fmt = fmt.lower()
@@ -56,30 +56,34 @@ def report(acct, path, fmt='txt'):
         print('Supported formats are ' + ','.join([f.upper() for f in VALID_FORMATS]))
         return
 
+    # Get account id
+    cur = db.cursor()
+    acct_id = cur.execute('SELECT acct_no FROM accounts WHERE name = "{}"'.format(acct)).fetchone()[0]
+
     if fmt == 'txt':
-        _report_txt(acct, path)
+        _report_txt(acct_id, acct, path)
     elif fmt == 'csv':
-        _report_csv(acct, path)
+        _report_csv(acct_id, acct, path)
     elif fmt == 'html':
-        _report_html(acct, path)
+        _report_html(acct_id, acct, path)
     elif fmt == 'pdf':
-        _report_pdf(acct, path)
+        _report_pdf(acct_id, acct, path)
     else:
         print('Unable to generate report')
         return
 
 
-def _report_txt(acct, path):
+def _report_txt(acct_id, acct_name, path):
+    _txt.generate_text_report(acct_id, acct_name, path)
+
+
+def _report_csv(acct_id, acct_name, path):
     pass
 
 
-def _report_csv(acct, path):
+def _report_html(acct_id, acct_name, path):
     pass
 
 
-def _report_html(acct, path):
-    pass
-
-
-def _report_pdf(acct, path):
+def _report_pdf(acct_id, acct_name, path):
     pass
