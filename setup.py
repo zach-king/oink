2d3e63afc2a805e183257f054ed427d16aff2087
Zachary King <kingzach77@gmail.com>
2018-07-08 18:02:09 -0500

    version bump; name change since fork

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='oink',
-      version='0.3.1',
+    name='oink-cli',
+      version='0.3.2',
       description='A CLI budgeting tools for nerds.',
       url='http://github.com/zach-king/oink',
-      download_url = 'https://github.com/zcking/oink/archive/0.3.1.tar.gz',
+      download_url = 'https://github.com/zcking/oink/archive/0.3.2.tar.gz',
       author='Zachary King',
       author_email='kingzach77@gmail.com',
       entry_points = {
@@ -22,4 +22,6 @@ setup(
         'tabulate==0.8.2',
       ],
       zip_safe=False,
+      python_requires='>=3.6',
+      license='MIT',
 )
